897256148bc654abcdf301be993cea8c3b55723b
Muditsri08 <mudit.srivastava@mymudra.com>
2026-03-28 20:43:53 +0530

    updated deployment errors

---
+++ config/db.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func ConnectDB() {
+	var connStr string
+
+	databaseURL := os.Getenv("DATABASE_URL")
+
+	if databaseURL != "" {
+		// Railway / Production
+		connStr = databaseURL + "?sslmode=require"
+	} else {
+		// Local DB (fallback)
+		connStr = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+		)
+	}
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error opening DB ❌", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error connecting to DB ❌", err)
+	}
+
+	DB = db
+	log.Println("Database connected ✅")
+}
